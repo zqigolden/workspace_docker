febfb43599712d6bb9b64dab29610cc0856556a1
qzhu <qzhu@gpu041.aibee.cn>
2019-10-18 16:45:51 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-IMAGE=registry.aibee.cn/aibee/qzhu/workspace-docker:0.0.16
+IMAGE=registry.aibee.cn/aibee/qzhu/workspace-docker:0.0.17
 docker build -t $IMAGE . &&
     docker push $IMAGE
