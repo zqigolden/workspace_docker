48b4bd784d5d53eab55ce4f4938a1ac0d2eb1948
QiZhu <qzhu@aibee.com>
2019-11-25 14:34:42 +0800

    更新bashrc_simple_alias.sh, README.md

--- bashrc_simple_alias.sh
+++ bashrc_simple_alias.sh
@@ -1,5 +1,8 @@
+
+#-------------CUSTOM_SIMPLE_ALIAS_START--------------------
 #From https://code.aibee.cn/qzhu/workspace-docker/raw/master/bashrc_simple_alias.sh
 ## iTerm2+Tmux alias
+export USE_CUSTOM_SIMPLE_ALIAS 1
 alias t='tmux -CC attach || tmux -CC new-session'
 alias ta='tmux -CC attach -t'
 alias tn='tmux -CC new-session'
@@ -16,4 +19,4 @@ alias vi='vim'
 c(){ cd "$1"; ls -lh --color=auto; }
 cn() { realpath $1 | tee /dev/stderr | it2copy; }
 hls() { hdfs dfs -ls $1 2>/dev/null | grep -v Found | awk "{ print $NF }"; }
-
+#-------------CUSTOM_SIMPLE_ALIAS_END--------------------
