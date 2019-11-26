b6f94022db7fa90ccdbb79f0985d6828ca394cab
QiZhu <qzhu@aibee.com>
2019-11-26 10:53:32 +0800

    Update bashrc_simple_alias.sh

--- bashrc_simple_alias.sh
+++ bashrc_simple_alias.sh
@@ -1,8 +1,7 @@
-
 #-------------CUSTOM_SIMPLE_ALIAS_START--------------------
 #From https://code.aibee.cn/qzhu/workspace-docker/raw/master/bashrc_simple_alias.sh
 ## iTerm2+Tmux alias
-export USE_CUSTOM_SIMPLE_ALIAS 1
+export USE_CUSTOM_SIMPLE_ALIAS=1
 alias t='tmux -CC attach || tmux -CC new-session'
 alias ta='tmux -CC attach -t'
 alias tn='tmux -CC new-session'
